bf1909e51cd953d6f56f3f7460258302ff23c35d
Mathy Vanvoorden <mathy@vanvoorden.be>
2024-08-09 10:40:45 +0200

    Make scripts work for both octopus and fox and generalize names

--- tools/flash.py
+++ tools/flash.py
@@ -3,15 +3,21 @@ import sys
 import glob
 import subprocess
 import time
+import argparse
 
 from serial.tools import list_ports
 
 # idVendor=303a, idProduct=1001
-VID = 0x303A
-PID = 0x1001
 
 
-def get_serial():
+def get_serial(badge):
+    if (badge == "fox"):
+        VID = 0x303A
+        PID = 0x1001
+    elif (badge == "octopus"):
+        VID = 0x10C4
+        PID = 0xEA60
+
     device_list = list_ports.comports()
     devices = []
     for device in device_list:
@@ -24,27 +30,37 @@ def get_serial():
     return devices
 
 
-seen_devices = []
-while True:
-    attached = get_serial()
-    for port in attached:
-        if port.serial_number not in seen_devices:
-            seen_devices.append(port.serial_number)
+def flash(badge):
+    seen_devices = []
+    while True:
+        attached = get_serial(badge)
+        for port in attached:
+            if port.serial_number not in seen_devices:
+                seen_devices.append(port.serial_number)
 
-            subprocess.Popen([
-                "esptool.py", "--chip", "esp32s3", "-b1500000", "-p", port.device, "--before", "default_reset", "--after",
-                "hard_reset",
-                "write_flash", "--erase-all", "--flash_mode", "dio", "--flash_freq", "80m", "--flash_size", "16MB",
-                "0x0", "images/bootloader.bin",
-                "0x8000", "images/partition-table.bin",
-                "0x9000", "images/ota_data_initial.bin",
-                "0x10000", "images/fri3d_firmware_fox.bin",
-                "0x210000", "images/fri3d_firmware_fox.bin",
-                "0x410000", "images/hwtest.bin",
-                "0x710000", "images/launcher.bin",
-                "0x810000", "images/retro-core.bin",
-                "0x8b0000", "images/prboom-go.bin",
-                "0x990000", "images/vfs.bin"
-            ]
-            )
-    time.sleep(1)
+                subprocess.Popen([
+                    "esptool.py", "--chip", "esp32s3", "-b1500000", "-p", port.device, "--before", "default_reset", "--after",
+                    "hard_reset",
+                    "write_flash", "--erase-all", "--flash_mode", "dio", "--flash_freq", "80m", "--flash_size", "16MB",
+                    "0x0", f"images/bootloader_{badge}.bin",
+                    "0x8000", f"images/partition_table_{badge}.bin",
+                    "0x9000", f"images/ota_data_{badge}.bin",
+                    "0x10000", f"images/fri3d_firmware_{badge}.bin",
+                    "0x210000", f"images/fri3d_firmware_{badge}.bin",
+                    "0x410000", f"images/micropython_{badge}.bin",
+                    "0x710000", f"images/retro_go_launcher_{badge}.bin",
+                    "0x810000", f"images/retro_go_core_{badge}.bin",
+                    "0x8b0000", f"images/retro_go_prboom_{badge}.bin",
+                    "0x990000", f"images/vfs_{badge}.bin"
+                ]
+                )
+        time.sleep(1)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('badge', nargs='?', default='fox', choices=['fox', 'octopus'])
+
+    args = parser.parse_args()
+
+    flash(args.badge)
--- tools/flash.sh
+++ tools/flash.sh
@@ -1,4 +1,21 @@
 #/bin/bash
 
-esptool.py --chip esp32s3 -p /dev/ttyACM0 -b1500000 --before=default_reset --after=hard_reset write_flash --erase-all --flash_mode dio --flash_freq 80m --flash_size 16MB 0x0 images/bootloader.bin 0x8000 images/partition-table.bin 0x9000 images/ota_data_initial.bin 0x10000 images/fri3d_firmware_fox.bin 0x210000 images/fri3d_firmware_fox.bin 0x410000 images/hwtest.bin 0x710000 images/launcher.bin 0x810000 images/retro-core.bin 0x8b0000 images/prboom-go.bin 0x990000 images/vfs.bin
+badge="${1:-fox}"
 
+case $badge in
+
+  fox)
+    device=/dev/ttyACM0
+    ;;
+
+  octopus)
+    device=/dev/ttyUSB0
+    ;;
+
+  *)
+    echo "Unknown type"
+    exit
+    ;;
+esac
+
+esptool.py --chip esp32s3 -p ${device} -b1500000 --before=default_reset --after=hard_reset write_flash --erase-all --flash_mode dio --flash_freq 80m --flash_size 16MB 0x0 images/bootloader_${badge}.bin 0x8000 images/partition_table_${badge}.bin 0x9000 images/ota_data_${badge}.bin 0x10000 images/fri3d_firmware_${badge}.bin 0x210000 images/fri3d_firmware_${badge}.bin 0x410000 images/micropython_${badge}.bin 0x710000 images/retro_go_launcher_${badge}.bin 0x810000 images/retro_go_core_${badge}.bin 0x8b0000 images/retro_go_prboom_${badge}.bin 0x990000 images/vfs_${badge}.bin
